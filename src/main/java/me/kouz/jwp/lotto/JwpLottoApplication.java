93f1e4c46a29289e061d20b0d8c79857a88987ae
Gyeongjun Kim <lastpuzly@gmail.com>
2021-03-16 19:53:07 +0900

    Initial commit

---
+++ src/main/java/me/kouz/jwp/lotto/JwpLottoApplication.java
@@ -0,0 +1,14 @@
+package me.kouz.jwp.lotto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.reactive.config.EnableWebFlux;
+
+@SpringBootApplication
+public class JwpLottoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwpLottoApplication.class, args);
+    }
+
+}
